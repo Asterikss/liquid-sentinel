99e910fc063c2f6f7c844f58a35b248a11ced9ae
Vitahu <vumirek@gmail.com>
2023-11-18 20:17:58 +0100

    wip scraper bs4

---
+++ scraper bs4.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import requests as req
+from bs4 import BeautifulSoup
+
+
+def get_bank_sites(url: str):
+    page = req.get(url, headers={
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    })
+    soup = BeautifulSoup(page.content, 'html.parser')
+    finds = soup.find_all('a', href=True)
+    finds = [find for find in finds if find['href'] != '#']
+    lokats = []
+    for find in finds:
+        parent = find.parent.parent
+        for child in parent.children:
+            if "lokat" in child.text.lower() and any(x in child.text.lower() for x in ("regul", "tabel")):
+                lokats.append(find['href'])
+    print(lokats)
+
+
+def main():
+    # sites = pd.read_csv(open("bank_sites.csv"), delimiter=";")
+    # sites['Osoby fizyczne'] = sites['Osoby fizyczne'].apply(lambda x: x.strip() if isinstance(x, str) else x)
+    # sites['Klienci korporacyjni'] = sites['Klienci korporacyjni'].apply(lambda x: x.strip() if isinstance(x, str) else x)
+    # for record in sites['Osoby fizyczne'].dropna():
+    #     for site in record.split(" ") if " " in record else [record]:
+    #         get_bank_sites(site)
+    get_bank_sites('https://www.velobank.pl/klienci-indywidualni/centrum-dokumentow')
+
+
+if __name__ == "__main__":
+    main()
