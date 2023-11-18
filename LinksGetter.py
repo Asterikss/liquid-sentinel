09c2e4d40119e7a2c3a2b350954e817b85414c8c
Cezym <s24418@pjwstk.edu.pl>
2023-11-18 13:54:05 +0100

    Scrapper 1.0

---
+++ LinksGetter.py
@@ -0,0 +1,50 @@
+# Klasa służąca do wczytywania danych z pliku
+import re
+
+
+class CSVReader:
+    # Utworzenie listy list (tabeli) danych na podstawie pliku
+    @staticmethod
+    def readCSV(path: str, split: str = ","):
+        with open(path) as file:
+            text = file.read()
+        return [line.split(split) for line in text.splitlines()]
+
+
+class LinksGetter:
+    @staticmethod
+    def getLink(text: str):
+        regex = re.compile(r'https:.+$')
+        br = re.compile(r'\[.*\]')
+        text = re.sub(br, "", text)
+        links: list[str] = regex.findall(text)
+        link = links[0]
+        links = link.split("https:")
+        links = ["https:" + link for link in links]
+        return [link for link in links[1:] if len(link) != 0]
+
+
+class PercLink:
+    def __init__(self, name: str, fiz: list[str], corp: list[str]):
+        self.name = name
+        self.fiz = fiz
+        self.corpo = corp
+
+    def __str__(self):
+        return self.name + "\n" + str(self.fiz) + "\n" + str(self.corpo) + "\n========"
+
+    @staticmethod
+    def getBanksFromTable():
+        csv = CSVReader.readCSV("tabele_oprocentowania.csv", split=";")
+        NAME_INDEX: int = 1
+        RIZZ_INDEX: int = 2
+        CORPO_INDEX: int = 3
+        percLinks = []
+        for i, row in enumerate(csv):
+            try:
+                percLinks.append(PercLink(row[NAME_INDEX],
+                                          LinksGetter.getLink(row[RIZZ_INDEX]),
+                                          LinksGetter.getLink(row[CORPO_INDEX] if len(row) > CORPO_INDEX else "")))
+            except IndexError:
+                pass
+        return percLinks
---
+++ WebScrapper.py
@@ -0,0 +1,34 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
+
+import LinksGetter
+
+
+class WebScrapper:
+
+    def __init__(self, url: str):
+        self.driver = webdriver.Chrome()
+        chrome_opt = Options()
+        chrome_opt.add_argument("--disable-extensions")
+        self.driver.get(url)
+
+if __name__ == "__main__":
+    banks = LinksGetter.PercLink.getBanksFromTable()
+    for bank in banks:
+        print(bank)
+    bank = banks[0]
+    ws = WebScrapper(bank.fiz[0])
+    allElements = ws.driver.find_elements(By.CSS_SELECTOR,"body *")
+    l = []
+    for element in allElements:
+        href = element.get_attribute("href")
+        if href and href.endswith(".pdf"):
+            print(href, element.text)
+            if len(element.text) > 0:
+                l.append((element.text, href))
+    for i in l:
+        print(i)
+    ws.driver.close()
+
