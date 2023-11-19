d639e33ee07c747eb1fa993d3dff4279b010c646
Asterikss <AndreAndreM@proton.me>
2023-11-19 03:22:09 +0100

    cleaned and updated pdf_iter

--- pdf_iter.py
+++ pdf_iter.py
@@ -1,68 +1,96 @@
+'''
+Iterate through collected PDFs, process text, shorten if too long
+(After identifying potential location of the interest rate)
+'''
 import os
 import pdfplumber
+from typing import List
 
-directory = 'bank_data'
-bad_words: list[str] = ["wycof", "nieakt"]
-length_max = 2000
-# private_list = []
-# bussiness_list = []
+directory = "bank_data"
+bad_words: List[str] = ["wycof", "nieakt"]
+length_max = 4000
 
+# Walk through all the files in bank_data directory
 for root, dirs, files in os.walk(directory):
     for filename in files:
-        whole_text = ""
-        # Perform operations with the file
-        # For example, print the file paths
+        print("~~~~")
+        # Collect information
         print(loc_path := os.path.join(root, filename))
         print(current_directory := os.path.join(root))
+
+        # Check if any of the titels of the PDFs contain words that might
+        # indicate that the information within them is outdated
         if all(bad_words not in loc_path for bad_words in bad_words):
             whole_text = ""
             with pdfplumber.open(loc_path) as pdf:
                 for page in pdf.pages:
                     text = page.extract_text()
                     whole_text += text
-                    # type(text)
-                    # print("here")
-                    # print(text)
-                    
 
-            print("here")
-            print(loc_path, "!!!!!")
+            # Write the contents of the file to the correct destination
+            # depending on the original location. Shorten if needed
             if "indywidualni" in loc_path:
-                print(loc_path[loc_path.find("/")+1: loc_path.find("/", loc_path.find("/") + 1)])
-                file_name = "parsed_data/" +  loc_path[loc_path.find("/")+1: loc_path.find("/", loc_path.find("/") + 1)] + "/indywidualni.txt"
-                print(file_name)
-                print(len(whole_text), "~!")
+                print(
+                    "Bank:", 
+                    loc_path[
+                        loc_path.find("/")
+                        + 1 : loc_path.find("/", loc_path.find("/") + 1)
+                    ]
+                )
+
+                file_name = (
+                    "parsed_data/"
+                    + loc_path[
+                        loc_path.find("/")
+                        + 1 : loc_path.find("/", loc_path.find("/") + 1)
+                    ]
+                    + "/indywidualni.txt"
+                )
+                print("destination:", file_name)
+                print("lenght of the document:", len(whole_text))
+
                 if len(whole_text) < length_max:
-                    print("here")
-                    with open(file_name, 'w') as file:
+                    print("kept as is (in private)")
+                    with open(file_name, "w") as file:
                         file.write(whole_text)
                 else:
-                    print("here2")
-                    whole_text = whole_text[whole_text.find("procentowan"):whole_text.find("procentowan")+length_max]
-                    with open(file_name, 'w') as file:
+                    print("shortened (in private)")
+                    whole_text = whole_text[
+                        whole_text.find("procentowan") : whole_text.find("procentowan")
+                        + length_max
+                    ]
+                    with open(file_name, "w") as file:
                         file.write(whole_text)
             else:
-                print("!!!!!!!!!!!!")
-                print(loc_path[loc_path.find("/")+1: loc_path.find("/", loc_path.find("/") + 1)])
-                file_name = "parsed_data/" +  loc_path[loc_path.find("/")+1: loc_path.find("/", loc_path.find("/") + 1)] + "/bussiness.txt"
-                print(file_name)
+                print(
+                    "Bank:",
+                    loc_path[
+                        loc_path.find("/")
+                        + 1 : loc_path.find("/", loc_path.find("/") + 1)
+                    ]
+                )
+                file_name = (
+                    "parsed_data/"
+                    + loc_path[
+                        loc_path.find("/")
+                        + 1 : loc_path.find("/", loc_path.find("/") + 1)
+                    ]
+                    + "/bussiness.txt"
+                )
+                print("destination:", file_name)
+                print("lenght of the document:", len(whole_text))
+
                 if len(whole_text) < length_max:
-                    print("here3")
-                    with open(file_name, 'w') as file:
+                    print("kept as is (in business)")
+                    with open(file_name, "w") as file:
                         file.write(whole_text)
                 else:
-                    print("here4")
-                    whole_text = whole_text[whole_text.find("procentowan"):whole_text.find("procentowan")+length_max]
-                    with open(file_name, 'w') as file:
+                    print("shortened (in business)")
+                    whole_text = whole_text[
+                        whole_text.find("procentowan") : whole_text.find("procentowan")
+                        + length_max
+                    ]
+                    with open(file_name, "w") as file:
                         file.write(whole_text)
-                # with open(file_name, 'w') as file:
-                    # file.write(whole_text)
-                # bussiness_list.append(loc_path)
 
-# updated_list = [desc for desc in l if not any(bad_word in desc[0] for bad_word in bad_words)]
-print("----")
-# print(private_list)
-# print("----")
-# print(bussiness_list)
-
-
+        print("~~~~")
