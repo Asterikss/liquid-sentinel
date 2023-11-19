361ba312205b972f050bbb6e5837f5443afe788c
Asterikss <AndreAndreM@proton.me>
2023-11-19 02:48:07 +0100

    clean feed_db

--- feed_db.py
+++ feed_db.py
@@ -1,67 +1,81 @@
+# Collect and process the data, so it is ready to be incorporated into a database
+# Then write it to a file
 import os
 import json
 from datetime import datetime
-# import locale
-# locale.setlocale(locale.LC_TIME, 'pl_PL.UTF-8')
-
-# date_str = "7 września 2023 r."
-# date_obj = datetime.strptime(date_str, "%d %B %Y").strftime("%d-%m-%Y")
-# print(date_obj)
 
 directory = "llm_parsed_data"
+
 polish_months = {
-    'stycznia': 'January',
-    'lutego': 'February',
-    'marca': 'March',
-    'kwietnia': 'April',
-    'maja': 'May',
-    'czerwca': 'June',
-    'lipca': 'July',
-    'sierpnia': 'August',
-    'września': 'September',
-    'października': 'October',
-    'listopada': 'November',
-    'grudnia': 'December'
+    "stycznia": "January",
+    "lutego": "February",
+    "marca": "March",
+    "kwietnia": "April",
+    "maja": "May",
+    "czerwca": "June",
+    "lipca": "July",
+    "sierpnia": "August",
+    "września": "September",
+    "października": "October",
+    "listopada": "November",
+    "grudnia": "December",
 }
 
+json_objects = []
+
+# walk through llm_parsed_data directory
 for root, dirs, files in os.walk(directory):
     for filename in files:
         print("~~")
 
-        print(loc_path := os.path.join(root, filename))
-        indywidualni = filename == "indywidualni.txt"
-        bank = root[root.find("/")+1:]
+        loc_path = os.path.join(root, filename)
+        is_private_offer = filename == "indywidualni.txt"
+        bank = root[root.find("/") + 1 :]
+        print(loc_path)
         print(bank)
-        print(indywidualni, "(indywidualni)")
+        print(is_private_offer, "(is private offer)")
+
+        # "Query" the last line of previously prepared files to get the date and the interest rate
         with open(loc_path, "r") as f:
             to_process = f.readlines()
             to_process = to_process[-1].strip()
-            data = to_process[:to_process.find(";")]
-            oprocentowanie = to_process[to_process.find(";") + 1: to_process.find("%") + 1].strip()
-            print(oprocentowanie)
-            print(data)
-            data = data.replace(" r.", "")
-            data = data.replace(" r", "")
-            # aaa = data.replace("września", "September")
+            date = to_process[: to_process.find(";")]
+            interest_rate = to_process[
+                to_process.find(";") + 1 : to_process.find("%") + 1
+            ].strip()
+            print(interest_rate)
+            print(date)
+            date = date.replace(" r.", "")
+            date = date.replace(" r", "")
+            print(date)
+
+            # process the data, depending on its form, so that it can be later easily parsable and queryable
             not_found = True
-            for pol_mon in polish_months.keys():
-                if pol_mon in data:
-                    data = data.replace(pol_mon, polish_months[pol_mon])
+            for pl_month in polish_months.keys():
+                if pl_month in date:
+                    date = date.replace(pl_month, polish_months[pl_month])
                     not_found = False
                     break
-            print(data)
+
+            print(date)
             if not_found:
-                date_obj = datetime.strptime(data, "%d.%m.%Y").strftime("%d-%m-%Y")
+                date_obj = datetime.strptime(date, "%d.%m.%Y").strftime("%d-%m-%Y")
             else:
-                date_obj = datetime.strptime(data, "%d %B %Y").strftime("%d-%m-%Y")
+                date_obj = datetime.strptime(date, "%d %B %Y").strftime("%d-%m-%Y")
             print(date_obj)
-            data = {
+            date = {
                 "bank_name": bank,
-                "pivate_offer": indywidualni,
-                "interest_rate": oprocentowanie,
-                "date": date_obj
+                "pivate_offer": is_private_offer,
+                "interest_rate": interest_rate,
+                "date": date_obj,
             }
-            json_data = json.dumps(data, ensure_ascii=False)
+            json_data = json.dumps(date, ensure_ascii=False)
+            json_objects.append(json_data)
             print(json_data)
 
         print("~~")
+
+with open("json_data/json_data.json", "w") as destination:
+    for obj in json_objects:
+        json.dump(obj, destination)
+        destination.write("\n")
